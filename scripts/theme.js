5db43f7d44cc11cd4a55bca6b6fbc478746c0dd2
Jonathan Cho <jonofcho@gmail.com>
2017-10-25 15:13:04 -0700

    rendition #3

--- scripts/theme.js
+++ scripts/theme.js
@@ -1,27 +1,14 @@
 $(document).ready(function(){
   var nav = $('.js-theme__nav');
   var navHeight = nav.height();
-
   if ($(window).scrollTop() < navHeight) {
     nav.removeClass('theme__nav--transition');
   }
 
-  $(window).on('scroll', function(){
-    var that = $(window);
-    var scrollPos = that.scrollTop();
-    var imageProfile = $('.js-hero-image__profile');
-    if (scrollPos > navHeight) {
-      nav.addClass('theme__nav--transition');
-      imageProfile.addClass('opacity-on');
-    }
-    else {
-      nav.removeClass('theme__nav--transition')
-      imageProfile.removeClass('opacity-on');
-    }
-  })
 
-  $('.js-pprint').on('click', function(){
-    $('.js-about__pprint').slideDown();
-  })
+  // $('.js-pprint').on('click', function(){
+  //   $('.js-about__print').;
+  //   $('.js-about__pprint').fadeIn();
+  // })
 
 })
